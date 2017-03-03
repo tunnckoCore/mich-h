a2ffcfdbce0f4a2f02b860aca6a8a85502eb9eba
tunnckoCore <mameto_100@mail.bg>
2017-03-03 21:05:38 +0200

    fix(nah): nah

--- test.js
+++ test.js
@@ -20,17 +20,17 @@ test('mich-h', (done) => {
 var ast = h('div#page.foo.bar.qux', { className: 'ok fool' },
   h('#header', // if tag name is not given, defaults to `div`
     h('h1.classy', null, { style: 'background-color: #333; color: purple' })),
-  h('nav#menu', { style: {'background': '#2f2', 'font-size': '12px' } },
+  h('nav#menu', { style: { 'background': '#2f2', 'font-size': '12px' } },
     h('ul', [
       h('li', 'one', { dataset: { foo: 'bar', set: 'ok' } }),
       h('li.sec', 'two', { className: ['huh'] }),
       h('li', 'three')
     ])),
-  h('h2#title', 'content title',  { style: {'background-color': 'red'} }),
+  h('h2#title', 'content title', { style: {'background-color': 'red'} }),
   h('p.first', // classes of that `p` would be `first, foobie`
     { className: 'foobie' },
     "so it's just like a templating engine,\n",
-    "but easy to use inline with javascript\n",
+    'but easy to use inline with javascript\n',
     { onclick: () => {} }
   ),
   h('.foo', {
@@ -39,14 +39,14 @@ var ast = h('div#page.foo.bar.qux', { className: 'ok fool' },
   }),
   h('p',
     { className: 'lastParagraph' },
-    "the intention is for this to be used to create\n",
+    'the intention is for this to be used to create\n',
     h('strong', 'charlike', {
       className: ['bold'],
       style: 'background: white; color: green'
     }),
-    " reusable, interactive html widgets. "))
+    ' reusable, interactive html widgets. '))
 
-// console.log(ast)
+console.log(ast)
 
 /**
  * Example Components
@@ -63,4 +63,4 @@ var foo = h(Foo, {
   last: 'Reagent'
 }, h('strong', { visible: true }, 'hello'))
 
-// console.log(foo)
+console.log(foo)
